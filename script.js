91bfa40e6fb8e9577da9d478e23041ef3d2f8612
Vasto <sonicdarkultra44@gmail.com>
2025-08-12 19:31:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("danger array")// DODGE.IO - SCRIPT.JS
+console.log("new highscore options")// DODGE.IO - SCRIPT.JS
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 
