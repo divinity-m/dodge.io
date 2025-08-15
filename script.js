c3a7d41a82159a0c31c032ee98be9f2fef501949
Vasto <sonicdarkultra44@gmail.com>
2025-08-15 10:27:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("drawAZRange")// DODGE.IO - SCRIPT.JS
+console.log("removed drawAZRange")// DODGE.IO - SCRIPT.JS
 const cnv = document.getElementById("canvas");
 const ctx = cnv.getContext('2d');
 
