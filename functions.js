b6132035466d3d9ac3e17fa498b1adbfe1e403ab
Vasto <sonicdarkultra44@gmail.com>
2025-08-16 00:45:50 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-console.log("UI improvements")// DODGE.IO - FUNCTIONS.JS
+console.log("dodger descriptions")// DODGE.IO - FUNCTIONS.JS
 function loadingScreen(validInput) {
     if (validInput || endLoading) {
         if (now - loadingGame >= 1000 && gameState == "loading") {
