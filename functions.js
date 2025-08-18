0d708e74fbad4d39caa2691a38393b98dbf636be
Vasto <sonicdarkultra44@gmail.com>
2025-08-18 15:40:22 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -582,34 +582,6 @@ function createCursor() {
     return cursor;
 }
 
-function drawCursor() {
-    if (settings.customCursor && cursorX && cursorY && lastPressing === "mouse") {
-        for (let i = allCursors.length-1; i >= 0; i--) {
-            if (allCursors[i].r <= 1/100 || trailDensity === 0.5) allCursors.splice(i, 1);
-        }
-        allCursors.forEach(cursor => {
-            let playerColor = player.color.slice(4, player.color.length-1);
-            ctxCursor.fillStyle = `rgba(${playerColor}, ${cursor.av})`;
-            ctxCursor.beginPath()
-            ctxCursor.arc(cursor.x, cursor.y, cursor.r, Math.PI * 2, 0)
-            ctxCursor.fill();
-            
-            cursor.r -= cursor.subR;
-            cursor.av -= cursor.subAv;
-        })
-        ctxCursor.fillStyle = player.color;
-        ctxCursor.beginPath()
-        ctxCursor.arc(cursorX, cursorY, 7.5, Math.PI * 2, 0)
-        ctxCursor.fill();
-        
-        ctxCursor.strokeStyle = player.subColor;
-        ctxCursor.lineWidth = 3;
-        ctxCursor.beginPath()
-        ctxCursor.arc(cursorX, cursorY, 7.5, Math.PI * 2, 0)
-        ctxCursor.stroke();
-    }
-}
-
 function drawStartScreen() {
     if (innerGameState === "mainMenu" || innerGameState === "selectDifficulty") {
         // Me
