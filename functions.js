7fce9d8a3e89113cae841ad341dfeb5dd1cfb550
Vasto <sonicdarkultra44@gmail.com>
2025-08-19 14:06:07 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@
     console.log("hello world");
 }
 sayHi();*/
-console.log("no-cursor is still buggy, dangers not showing")
+console.log("no-cursor is still buggy, dangers not showing, undefined timestamps fix")
 
 function loadingScreen(validInput) {
     if (validInput || endLoading) {
