18233542c1adc681cc97f57ac581105e724c2300
Vasto <sonicdarkultra44@gmail.com>
2025-08-20 15:02:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO - SCRIPT.JS
-console.log("canvas Y offset")
+console.log("canvas offset not updating?")
 const cnv = document.getElementById("game");
 const ctx = cnv.getContext('2d');
 
