3d7d886b5d0d7683328429a645328d3aa9d7b567
Vasto <sonicdarkultra44@gmail.com>
2025-09-23 21:16:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DODGE.IO - SCRIPT.JS
-console.log("invincibility shield")
+console.log("invincibility shield rescaled")
 const cnv = document.getElementById("game");
 const ctx = cnv.getContext('2d');
 
