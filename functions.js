86a6c1914975722e88159ec9c91abad12b198615
Vasto <sonicdarkultra44@gmail.com>
2025-10-02 18:41:55 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -662,41 +662,48 @@ function createClick(button) {
 }
 
 function drawStartScreen() {
-    if (innerGameState === "mainMenu" || innerGameState === "selectDifficulty") {
-        // Main Menu Background Animation
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "rgb(170, 170, 170)";
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(GAME_WIDTH, GAME_HEIGHT);
-        ctx.stroke();
+    // Line across the screen
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "rgb(170, 170, 170)";
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(GAME_WIDTH, GAME_HEIGHT);
+    ctx.stroke();
 
-        ctx.fillStyle = "rgb(170, 170, 170)";
-        ctx.font = '150px Arial';
-        ctx.textAlign = 'center';
+    // Main Menu Background Animation
+    ctx.fillStyle = "rgb(170, 170, 170)";
+    ctx.font = '150px Arial';
+    ctx.textAlign = 'center';
 
-        let hyp = Math.hypot(GAME_WIDTH, GAME_HEIGHT);
-        ctx.save();
-        ctx.rotate(Math.atan(GAME_HEIGHT/GAME_WIDTH));
-        ctx.fillText("MAIN", BgTopX, 0);
-        ctx.restore();
-        
-        ctx.save();
-        ctx.rotate(Math.atan(GAME_HEIGHT/GAME_WIDTH));
-        ctx.fillText("MENU", BgBottomX, 103);
-        ctx.restore();
+    // top text
+    ctx.save();
+    ctx.rotate(Math.atan(GAME_HEIGHT/GAME_WIDTH));
+    ctx.fillText(bgTopText, bgTopX, 0);
+    ctx.restore();
 
-        if (BgTopX <= hyp*4/10) BgTopX += 10 * Math.max(0.5, (2 - (now - BgTime)/1000));
-        
-        if (BgBottomX >= hyp*6/10 && now - BgTime > 1000) BgBottomX -= 10 * Math.max(0.5, (3 - (now - BgTime)/1000));
-        
+    // bottom text
+    ctx.save();
+    ctx.rotate(Math.atan(GAME_HEIGHT/GAME_WIDTH));
+    ctx.fillText(bgBottomText, bgBottomX, 103);
+    ctx.restore();
+
+    // Subtracs the current X from the destination, then divides that number by the destination and multiplies it by 100
+    let dBgTopX = 100 * Math.max(0.01, (bgTopMax - bgTopX) / bgTopMax);
+    let dBgBottomX = 100 * Math.mix(-0.01, (bgBottomMax - bgBottomX) / bgBottomMax);
+    
+    if (bgTopX <= hyp*4/10) bgTopX += dBgTopX;
+    if (bgBottomX >= hyp*6/10 && bgTopX <= hyp*4/10 - 25) bgBottomX += dBgBottomX;
+
+    if (innerGameState === "mainMenu") {
         // Me
         ctx.strokeStyle = player.color;
         ctx.lineWidth = 1.5;
         ctx.font = '30px Roboto';
         ctx.textAlign = 'left';
         ctx.strokeText("Vasto", 5, 30);
-        
+    }
+
+    if (innerGameState === "mainMenu" || innerGameState === "selectDifficulty") {
         // PLAY BUTTON //
         const playBtn = {
             x: 250,
